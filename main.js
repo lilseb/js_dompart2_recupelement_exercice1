cfc446220a0e85c915bf7348d53a44d55bc85459
Sébastien <seb.lasante@hotmail.com>
2021-01-29 10:15:46 +0100

    fini

---
+++ main.js
@@ -0,0 +1,14 @@
+//1 par la class
+let classTartine = document.getElementsByClassName('tartine')
+console.log(classTartine[0].innerText);
+
+//2 par l'id
+let idToast = document.getElementById('toast')
+console.log(idToast.innerText);
+
+//3
+console.log(classTartine[1]);
+
+
+// par la balise
+let titre = document.getElementsByTagName('h1')
